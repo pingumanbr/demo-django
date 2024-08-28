f48d618d0a1099c3d6a6c3b74d257551a87f8ce3
Manoel Dantas <manoel.dantas@gmail.com>
2024-08-28 17:26:04 -0300

    Update wsgi.py

--- geo/wsgi.py
+++ geo/wsgi.py
@@ -8,12 +8,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os,sys
-
-sys.path.append('geo_root/geo')
-sys.path.append('geo_root')
-
 from django.core.wsgi import get_wsgi_application
-#os.environ.setdefault('PYTHONPATH','/Users/manoel.dantas/miniforge3/lib/python3.10/site-packages')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geo.settings')
-
 application = get_wsgi_application()
